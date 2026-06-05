6f0309dec5561d96d350cfa84b7912a89013b389
zkh128 <3373418624@qq.com>
2026-06-05 20:15:55 +0800

    RICE v2.0 版本号

--- picoc/picoc.h
+++ picoc/picoc.h
@@ -8,9 +8,9 @@
 
 /* picoc version number */
 #ifdef VER
-#define PICOC_VERSION "v2.2 beta r" VER         /* VER is the subversion version number, obtained via the Makefile */
+#define PICOC_VERSION "v2.0 beta r" VER         /* VER is the subversion version number, obtained via the Makefile */
 #else
-#define PICOC_VERSION "v2.2"
+#define PICOC_VERSION "v2.0"
 #endif
 
 /* handy definitions */
